58788ffbd6f510b4541368a021d9feef987df5f6
VlodyaKr <vlodya@gmail.com>
2022-06-25 09:23:30 +0300

    Add function
    Add:
    show date
    change note
    delete note
    find note

--- notebook.py
+++ notebook.py
@@ -2,7 +2,7 @@
 import datetime
 import pickle
 from collections import UserDict
-# from datetime import date
+from datetime import date
 from pathlib import Path
 
 N = 3  # Кількість нотаток на сторінці
@@ -24,8 +24,8 @@ class InputError:
         #     return 'Error! Give me name and phone or birthday please!'
         except KeyError:
             return 'Error! Note not found!'
-        # except ValueError:
-        #     return 'Error! Phone number is incorrect!'
+        except ValueError:
+            return 'Error! Number of days is incorrect!'
         except DateIsNotValid:
             return 'Error! Date is not valid'
 
@@ -40,6 +40,18 @@ class Field:
 
     def __eq__(self, other) -> bool:
         return self.value == other.value
+
+    def __lt__(self, other) -> bool:
+        return self.value < other.value
+
+    def __gt__(self, other) -> bool:
+        return self.value > other.value
+
+    def __le__(self, other) -> bool:
+        return self.value <= other.value
+
+    def __ge__(self, other) -> bool:
+        return self.value >= other.value
 
 
 class ExecDate(Field):
@@ -150,6 +162,24 @@ def add_note(notebook, *args):
 
 
 @InputError
+def change_note(notebook, *args):
+    id_note, new_text = int(args[0]), ' '.join(args[1:])
+    notebook[id_note].text = new_text
+    return f'Note ID:{id_note} changed'
+
+
+@InputError
+def del_note(notebook, *args):
+    id_note = int(args[0])
+    yes_no = input(f'Are you sure you want to delete the note ID:{id_note}? (Y/n) ')
+    if yes_no == 'Y':
+        del notebook[id_note]
+        return f'Note ID:{id_note} deleted'
+    else:
+        return 'Note not deleted'
+
+
+@InputError
 def add_date(notebook, *args):
     """Додає дату нотатки"""
     id_note, exec_date = int(args[0]), args[1]
@@ -180,6 +210,45 @@ def show_archiv(notebook, *args):
     print_list = notebook.iterator(filter_func)
     if not print_list:
         return 'Archive is empty'
+    for item in print_list:
+        result += f'{item}'
+    return result
+
+
+def find_note(notebook, *args):
+    """Повертає нотатки за входженням в текст"""
+    def filter_func(note):
+        return subtext in note.text
+
+    subtext = args[0]
+    result = f'List of notes with text "{subtext}":\n'
+    print_list = notebook.iterator(filter_func)
+    if not print_list:
+        return 'List is empty'
+    for item in print_list:
+        result += f'{item}'
+    return result
+
+
+@InputError
+def show_date(notebook, *args):
+    """Повертає нотатки з вказаною датою виконання"""
+    def filter_func(note):
+        if note.exec_date is None:
+            return False
+        date1 = (date_find.value - datetime.timedelta(days=days)).strftime('%Y-%m-%d')
+        date2 = (date_find.value + datetime.timedelta(days=days)).strftime('%Y-%m-%d')
+        return ExecDate(date1) <= note.exec_date <= ExecDate(date2)
+
+    date_find = ExecDate(args[0])
+    if len(args) > 1:
+        days = int(args[1])
+    else:
+        days = 0
+    result = 'List of notes with date:\n'
+    print_list = notebook.iterator(filter_func)
+    if not print_list:
+        return 'Notebook is empty'
     for item in print_list:
         result += f'{item}'
     return result
@@ -221,17 +290,22 @@ def help_me(*args):
     return """Command format:
     help or ? - this help;
     add note <text> - add note;
-    add date id <date> - add date;
+    change note <id> <text> - change note;
+    delete note <id> - delete note;
+    add date id <date> - add/change date;
     done id - mark note as done;
     return id - mark note as not done;
     show all - show all notes;
     show archived - show archived notes;
+    show date <date> [<days>] - show notes by date +- days;
+    find note <text> - find note by text;
     good bye or close or exit or . - exit the program"""
 
 
 COMMANDS = {help_me: ['?', 'help'], goodbye: ['good bye', 'close', 'exit', '.'], add_note: ['add note '],
             add_date: ['add date '], show_all: ['show all'], show_archiv: ['show archived'],
-            done_note: ['done '], return_note: ['return ']}
+            change_note: ['change note '], del_note: ['delete note '], find_note: ['find note '],
+            show_date: ['show date '], done_note: ['done '], return_note: ['return ']}
 
 
 def command_parser(user_command: str) -> (str, list):
